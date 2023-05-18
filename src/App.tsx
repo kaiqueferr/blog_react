59542c7982d7d84ea5c711d0ab6d4b5676982614
Kaique <kaique.ferr@gmail.com>
2023-05-18 14:18:39 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './paginas/home/Home';
 
 function App() {
   return (
-    <p>Hello Woeld!</p>
+    <Home />
   );
 }
 
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+        <p>Home!</p>
+        <img src="https://media.tenor.com/cEgMDd-0nwQAAAAC/salve-salve-familia-flow.gif" alt="" />
+    </>
+
+  );
+}
+
+export default Home;
